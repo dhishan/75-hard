aacc07c42c4e79cc3b8ab811072257d0127eb69b
Dhishan Amaranath <iamdhishan@gmail.com>
2026-03-30 16:19:00 -0700

    feat: enable Google sign-in provider in Firebase Auth

--- terraform/main/firebase.tf
+++ terraform/main/firebase.tf
@@ -24,6 +24,10 @@ resource "google_identity_platform_config" "auth" {
       enabled           = true
       password_required = true
     }
+
+    google {
+      enabled = true
+    }
   }
 
   depends_on = [google_firebase_project.default]
