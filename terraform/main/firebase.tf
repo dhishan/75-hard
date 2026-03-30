d8f25ea3c3efd81a865375863e8769f2cf0bd70a
Dhishan Amaranath <iamdhishan@gmail.com>
2026-03-30 16:17:27 -0700

    fix: add authorized domains to Firebase Auth config

--- terraform/main/firebase.tf
+++ terraform/main/firebase.tf
@@ -11,6 +11,12 @@ resource "google_firebase_project" "default" {
 resource "google_identity_platform_config" "auth" {
   project = var.project_id
 
+  authorized_domains = [
+    "localhost",
+    "personal-projects-473219.firebaseapp.com",
+    "75hard.${var.root_domain}",
+  ]
+
   sign_in {
     allow_duplicate_emails = false
 
